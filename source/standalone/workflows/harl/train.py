26baeb3f41e2bd8709d2e51660b4b7eaa861c235
some45bucks <jhaightsnake@gmail.com>
2024-10-29 16:37:23 -0600

    added in env tools switcher for harl

--- source/standalone/workflows/harl/train.py
+++ source/standalone/workflows/harl/train.py
@@ -5,7 +5,7 @@ import json
 
 from omni.isaac.lab.app import AppLauncher
 
-parser = argparse.ArgumentParser(description="Train an RL agent with RL-Games.")
+parser = argparse.ArgumentParser(description="Train an RL agent with HARL.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
 parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
 parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
@@ -15,47 +15,8 @@ parser.add_argument("--seed", type=int, default=None, help="Seed used for the en
 parser.add_argument(
     "--distributed", action="store_true", default=False, help="Run training with multiple GPUs or nodes."
 )
-parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint.")
-parser.add_argument("--sigma", type=str, default=None, help="The policy's initial standard deviation.")
 parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
-
-# append AppLauncher cli args
-AppLauncher.add_app_launcher_args(parser)
-# parse the arguments
-args_cli, hydra_args = parser.parse_known_args()
-# always enable cameras to record video
-if args_cli.video:
-    args_cli.enable_cameras = True
-
-# clear out sys.argv for Hydra
-sys.argv = [sys.argv[0]] + hydra_args
-
-# launch omniverse app
-app_launcher = AppLauncher(args_cli)
-
-from harl.utils.configs_tools import get_defaults_yaml_args, update_args
-
-from omni.isaac.lab.envs import (
-    DirectMARLEnv,
-    DirectMARLEnvCfg,
-    DirectRLEnvCfg,
-    ManagerBasedRLEnvCfg,
-    multi_agent_to_single_agent,
-)
-from omni.isaac.lab.utils.assets import retrieve_file_path
-from omni.isaac.lab.utils.dict import print_dict
-from omni.isaac.lab.utils.io import dump_pickle, dump_yaml
-
-import omni.isaac.lab_tasks  # noqa: F401
-from omni.isaac.lab_tasks.utils.hydra import hydra_task_config
-from omni.isaac.lab_tasks.utils.wrappers.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
-
-def main():
-    """Main function."""
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument(
+parser.add_argument(
         "--algo",
         type=str,
         default="happo",
@@ -73,69 +34,55 @@ def main():
         ],
         help="Algorithm name. Choose from: happo, hatrpo, haa2c, haddpg, hatd3, hasac, had3qn, maddpg, matd3, mappo.",
     )
-    parser.add_argument(
-        "--env",
-        type=str,
-        default="pettingzoo_mpe",
-        choices=[
-            "smac",
-            "mamujoco",
-            "pettingzoo_mpe",
-            "gym",
-            "football",
-            "dexhands",
-            "smacv2",
-            "lag",
-        ],
-        help="Environment name. Choose from: smac, mamujoco, pettingzoo_mpe, gym, football, dexhands, smacv2, lag.",
-    )
-    parser.add_argument(
-        "--exp_name", type=str, default="installtest", help="Experiment name."
-    )
-    parser.add_argument(
-        "--load_config",
-        type=str,
-        default="",
-        help="If set, load existing experiment config file instead of reading from yaml config file.",
-    )
-    args, unparsed_args = parser.parse_known_args()
 
-    def process(arg):
-        try:
-            return eval(arg)
-        except:
-            return arg
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli, hydra_args = parser.parse_known_args()
+# always enable cameras to record video
+if args_cli.video:
+    args_cli.enable_cameras = True
 
-    keys = [k[2:] for k in unparsed_args[0::2]]  # remove -- from argument
-    values = [process(v) for v in unparsed_args[1::2]]
-    unparsed_dict = {k: v for k, v in zip(keys, values)}
-    args = vars(args)  # convert to dict
-    if args["load_config"] != "":  # load config from existing config file
-        with open(args["load_config"], encoding="utf-8") as file:
-            all_config = json.load(file)
-        args["algo"] = all_config["main_args"]["algo"]
-        args["env"] = all_config["main_args"]["env"]
-        algo_args = all_config["algo_args"]
-        env_args = all_config["env_args"]
-    else:  # load config from corresponding yaml file
-        algo_args, env_args = get_defaults_yaml_args(args["algo"], args["env"])
-    update_args(unparsed_dict, algo_args, env_args)  # update args from command line
+# clear out sys.argv for Hydra
+sys.argv = [sys.argv[0]] + hydra_args
 
-    if args["env"] == "dexhands":
-        import isaacgym  # isaacgym has to be imported before PyTorch
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
 
-    # note: isaac gym does not support multiple instances, thus cannot eval separately
-    if args["env"] == "dexhands":
-        algo_args["eval"]["use_eval"] = False
-        algo_args["train"]["episode_length"] = env_args["hands_episode_length"]
+import gymnasium as gym
+import os
+import random
+from datetime import datetime
+from harl.runners import RUNNER_REGISTRY
 
-    # start training
-    from harl.runners import RUNNER_REGISTRY
+from omni.isaac.lab.envs import (
+    DirectMARLEnv,
+    DirectMARLEnvCfg,
+    DirectRLEnvCfg,
+    ManagerBasedRLEnvCfg,
+    multi_agent_to_single_agent,
+)
+from omni.isaac.lab.utils.assets import retrieve_file_path
+from omni.isaac.lab.utils.dict import print_dict
+from omni.isaac.lab.utils.io import dump_pickle, dump_yaml
 
-    runner = RUNNER_REGISTRY[args["algo"]](args, algo_args, env_args)
+import omni.isaac.lab_tasks  # noqa: F401
+from omni.isaac.lab_tasks.utils.hydra import hydra_task_config
+
+agent_cfg_entry_point = "harl_cfg_entry_point"
+
+@hydra_task_config(args_cli.task, agent_cfg_entry_point)
+def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: dict):
+
+
+    agent_cfg["eval"]["use_eval"] = False
+
+    runner = RUNNER_REGISTRY[args_cli.algo](args_cli, agent_cfg, env_cfg)
     runner.run()
     runner.close()
 
 
 if __name__ == "__main__":
     main()
+    simulation_app.close()
