50a590256fc056eece20a956b4906f4664fa4e28
Isaac Peterson <isaac.peterson@usu.edu>
2025-02-19 16:05:26 -0700

    progress on acreating a training env for walking with h1

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/h1_walking_harl/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/h1_walking_harl/__init__.py
@@ -39,3 +39,16 @@ gym.register(
         "harl_ppo_cfg_entry_point": f"{agents.__name__}:harl_ppo_cfg.yaml"
     },
 )
+
+gym.register(
+    id="Isaac-H1-Velocity-Direct-v0",
+    entry_point=f"{__name__}.h1_velocity_env:H1VelocityEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.h1_velocity_env:H1VelocityEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "harl_ppo_cfg_entry_point": f"{agents.__name__}:harl_ppo_cfg.yaml"
+    },
+)
